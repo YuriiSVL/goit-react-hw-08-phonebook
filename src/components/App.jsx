04126935d36db290aecfece2f393c50b8417d4e2
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-05-11 01:59:58 +0300

    step 6 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,13 @@ import Filter from './Felter/Felter';
 
 export class App extends React.Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    // contacts: [],
     filter: '',
   };
 
@@ -40,6 +46,12 @@ export class App extends React.Component {
     return visibleContacts;
   };
 
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
   render() {
     const visibleContacts = this.getVisibleContacts();
     return (
@@ -48,7 +60,10 @@ export class App extends React.Component {
         <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
         <Filter value={this.state.filter} onChange={this.changeFilter} />
-        <ContactList contacts={visibleContacts} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
     );
   }
