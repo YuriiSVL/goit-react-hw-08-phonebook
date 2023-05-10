e69aa20b25413978f8de7087da96a7780847055b
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-05-11 01:40:20 +0300

    step 4,5 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactFrom/ContactFrom';
 import ContactList from './ContactList/ContactList';
+import Filter from './Felter/Felter';
 
 export class App extends React.Component {
   state = {
@@ -15,18 +16,39 @@ export class App extends React.Component {
       name: data.name,
       number: data.number,
     };
+    const alreadyAddedContact = this.state.contacts.find(
+      contact => data.name === contact.name
+    );
+    if (alreadyAddedContact) {
+      alert(`${alreadyAddedContact.name} is already in contacts`);
+      return;
+    }
     this.setState(prevState => ({
       contacts: [contact, ...prevState.contacts],
     }));
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const visibleContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+    return visibleContacts;
+  };
+
   render() {
+    const visibleContacts = this.getVisibleContacts();
     return (
       <div>
         <h2>Phonebook</h2>
         <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
-        <ContactList contacts={this.state.contacts} />
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <ContactList contacts={visibleContacts} />
       </div>
     );
   }
