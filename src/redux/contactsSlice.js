74c72e77da5c6a6cb40f78571ba56cc5ad6c55a0
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-06-24 04:29:29 +0300

    init

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,15 +1,5 @@
-// import { nanoid } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
-// import axios from 'axios';
-// import { createAsyncThunk } from '@reduxjs/toolkit';
 import { fetchContacts, deleteContact, addContact } from './operations';
-
-// axios.defaults.baseURL = 'https://648f72eb75a96b66444525b8.mockapi.io';
-
-// export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-//   const response = await axios.get('/contacts');
-//   return response.data;
-// });
 
 const initialContactsState = {
   contacts: [],
@@ -29,29 +19,6 @@ const handleRejected = (state, action) => {
 const contactsSlise = createSlice({
   name: 'contacts',
   initialState: initialContactsState,
-  // Об'єкт редюсерів
-  // reducers: {
-  //   addContact: {
-  //     reducer(state, action) {
-  //       state.contacts.push(action.payload);
-  //     },
-  //     prepare(name, number) {
-  //       return {
-  //         payload: {
-  //           id: nanoid(),
-  //           name,
-  //           number,
-  //         },
-  //       };
-  //     },
-  //   },
-  //   deleteContact(state, action) {
-  //     const index = state.contacts.findIndex(
-  //       contact => contact.id === action.payload
-  //     );
-  //     state.contacts.splice(index, 1);
-  //   },
-  // },
 
   extraReducers: {
     [fetchContacts.pending]: handlePending,
@@ -78,20 +45,6 @@ const contactsSlise = createSlice({
       state.contacts.splice(index, 1);
     },
     [deleteContact.rejected]: handleRejected,
-    // [toggleCompleted.pending]: handlePending,
-    // [toggleCompleted.fulfilled](state, action) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   const index = state.contacts.findIndex(
-    //     task => task.id === action.payload.id
-    //   );
-    //   state.contacts.splice(index, 1, action.payload);
-    // },
-    // [toggleCompleted.rejected]: handleRejected,
   },
 });
-
-// Генератори екшенів
-// export const { addContact, deleteContact } = contactsSlise.actions;
-// Редюсер слайсу
 export const contactsReducer = contactsSlise.reducer;
